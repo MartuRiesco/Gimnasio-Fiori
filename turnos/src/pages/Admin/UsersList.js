552ec1db45193304f0c7c205837234e2ccccaf38
martin-centurion <martinalejandrocenturion@gmail.com>
2024-06-04 15:39:07 -0300

    delete admin

--- turnos/src/pages/Admin/UsersList.js
+++ turnos/src/pages/Admin/UsersList.js
@@ -69,50 +69,52 @@ function UsersList() {
         </Radio.Group>
       </div>
       <div className='service-container'>
-        {filteredUsers.map((user) => (
-          <div className="employee-card" key={user._id}>
-            <h2>{user.name}</h2>
-            <div className="employee-container">
-              <Link
-                className="employee-whatsapp"
-                to={`https://api.whatsapp.com/send?phone=549${user.phone}`}
-              >
-                <i className="ri-whatsapp-line"></i>
-                {user.phone}
-              </Link>
-            </div>
-            {user.isEmployee ? (
-              <p>Empleado</p>
+  {filteredUsers.map((user) => (
+    !user.isAdmin && (
+      <div className="employee-card" key={user._id}>
+        <h2>{user.name}</h2>
+        <div className="employee-container">
+          <Link
+            className="employee-whatsapp"
+            to={`https://api.whatsapp.com/send?phone=549${user.phone}`}
+          >
+            <i className="ri-whatsapp-line"></i>
+            {user.phone}
+          </Link>
+        </div>
+        {user.isEmployee ? (
+          <p>Empleado</p>
+        ) : (
+          <div>
+            <p>Mes de inscripción: {user.mesInscripcion}</p>
+            <p>Observaciones: {user.observaciones}</p>
+            <p>Fecha de pago: {user.fechaPago ? moment(user.fechaPago).format('DD-MM-YYYY') : 'N/A'}</p>
+            <p>Monto abonado: ${user.monto ? user.monto : 'N/A'}</p>
+            {user.pago ? (
+              <h1 className="employee-approved">Pago</h1>
             ) : (
-              <div>
-                <p>Mes de inscripción: {user.mesInscripcion}</p>
-                <p>Observaciones: {user.observaciones}</p>
-                <p>Fecha de pago: {user.fechaPago ? moment(user.fechaPago).format('DD-MM-YYYY') : 'N/A'}</p>
-                <p>Monto abonado: ${user.monto ? user.monto : 'N/A'}</p>
-                {user.pago ? (
-                  <h1 className="employee-approved">Pago</h1>
-                ) : (
-                  <h1 className="employee-rejected">No Pago</h1>
-                )}
-              </div>
+              <h1 className="employee-rejected">No Pago</h1>
             )}
-            <div>
-              <h1
-                className="employee-change"
-                onClick={() => navigate(`/get-user-info-by-id/${user._id}`)}
-              >
-                Actualizar Usuario
-              </h1>
-              <h1
-                className="employee-rejected"
-                onClick={() => deleteUser(user._id)}
-              >
-                Borrar Usuario
-              </h1>
-            </div>
           </div>
-        ))}
+        )}
+        <div>
+          <h1
+            className="employee-change"
+            onClick={() => navigate(`/get-user-info-by-id/${user._id}`)}
+          >
+            Actualizar Usuario
+          </h1>
+          <h1
+            className="employee-rejected"
+            onClick={() => deleteUser(user._id)}
+          >
+            Borrar Usuario
+          </h1>
+        </div>
       </div>
+    )
+  ))}
+</div>
     </div>
   );
 }
